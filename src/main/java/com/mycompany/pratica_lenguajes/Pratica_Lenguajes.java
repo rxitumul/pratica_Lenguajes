b437dc0fc4d311fa83cb14e040ed3cd5c8a45629
rxitumul <richi22castillox@gmail.com>
2026-08-07 12:11:38 -0600

    inicio lenguajes

---
+++ src/main/java/com/mycompany/pratica_lenguajes/Pratica_Lenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pratica_lenguajes;
+
+/**
+ *
+ * @author ricardocastillo
+ */
+public class Pratica_Lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
